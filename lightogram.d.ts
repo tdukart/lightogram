d18e03054bc8b1c5d63f90e5a8a45853ef25dcba
Todd Dukart <tdukart@aloftinteractive.com>
2016-10-02 23:57:42 -0400

    Update typings

--- lightogram.d.ts
+++ lightogram.d.ts
@@ -1,101 +1,70 @@
-/**
- * Creates a Bridge based on the given data.
- * @param {HueBridgeConfigurationRead} bridgeData The bridge's data.
- * @param {string} appName                        The name of the app.
- * @param {string} deviceName                     A name of the device the app is run on.
- *
- */
-declare class LightogramBridge {
-    /**
-     * Creates a Bridge based on the given data.
-     * @param {HueBridgeConfigurationRead} bridgeData The bridge's data.
-     * @param {string} appName                        The name of the app.
-     * @param {string} deviceName                     A name of the device the app is run on.
-     *
-     */
+interface HueLightStateRead {
+    on:boolean,
+    bri?:number,
+    hue?:number,
+    sat?:number,
+    xy?:[number,number],
+    ct?:number,
+    alert?:"none"|"select"|"lselect",
+    effect?:"none"|"colorloop",
+    colormode?:"hs"|"xy"|"ct",
+    reachable:boolean
+}
+
+interface HueLightStateSet {
+    on?:boolean,
+    bri?:number,
+    hue?:number,
+    sat?:number,
+    xy?:[number,number],
+    ct?:number,
+    alert?:"none"|"select"|"lselect",
+    effect?:"none"|"colorloop"
+    transitiontime?:number,
+    bri_inc?:number,
+    sat_inc?:number,
+    hue_inc?:number,
+    ct_inc?:number,
+    xy_inc?:number
+}
+
+interface HueBridgeConfigurationRead {
+    name:string,
+    swupdate:Object,
+    whitelist:{[id:string]:{'last use date':string, 'create date':string, 'name':string}},
+    apiversion:string,
+    swversion:string,
+    proxyaddress:string,
+    proxyport:number,
+    linkbutton:boolean,
+    ipaddress:string,
+    mac:string,
+    netmask:string,
+    gateway:string,
+    dhcp:boolean,
+    portalservices:boolean,
+    UTC:string,
+    localtime:string,
+    timezone:string,
+    zibgeechannel:string,
+    modelid:string,
+    bridgeid:string,
+    factorynew:boolean,
+    replacesbridgeid:string
+}
+
+interface LightogramBridge {
     constructor(bridgeData: HueBridgeConfigurationRead, appName: string, deviceName: string);
-
-    /**
-     * Finds the given bridge.
-     * @returns {Promise}
-     */
     findBridge(): Promise;
-
-    /**
-     * Performs an API call.
-     * @param {string} action     An HTTP action.
-     * @param {string} [endpoint] The bridge-specific endpoint.
-     * @param {Object} [data]     The data to send in the request body.
-     * @returns {Promise<*>}
-     */
     doApiCall(action: string, endpoint?: string, data?: Object): Promise<any>;
-
-    /**
-     * Constructs an endpoint URL.
-     * @param {string} endpoint The endpoint.
-     * @private
-     * @returns {string} The full URL.
-     */
-    private _constructEndpointUrl(endpoint: string): string;
-
-    /**
-     * Gets the IP address of the bridge.
-     * @returns {Promise<string>}
-     */
     getIpAddress(): Promise<string>;
-
-    /**
-     * Gets the username of the bridge.
-     * @returns {Promise<string>}
-     */
     getUsername(): Promise<string>;
-
-    /**
-     * Gets the bridge's configuration.
-     * @param {boolean} [forceRefresh] If true, forces a refresh of the cached data.
-     * @returns {Promise}
-     */
     getConfig(forceRefresh?: boolean): Promise;
-
-    /**
-     * Gets the name of the bridge.
-     * @returns {Promise}
-     */
     getName(): Promise;
-
-    /**
-     * Retrieves the Light object given the ID.
-     * @param {*} lightId The light's ID.
-     * @returns {Light}
-     */
     light(lightId: any): Light;
-
-    /**
-     * Sets the state of a light.
-     * @deprecated Use the light's methods instead.
-     * @param {*} lightId
-     * @param {*} state
-     * @returns {*}
-     */
     setLightState(lightId: any, state: any): any;
-
-    /**
-     * Revokes the authorization of a given username.
-     * @param username
-     * @returns {Promise}
-     */
     removeAuthorization(username: any): Promise;
-
-    /**
-     * Periodically queries the bridge to ask for authorization.
-     * @returns {Promise} Resolved when the authorization is approved, or rejected if 60 seconds have passed with no authorization.
-     */
     waitForAuthorization(): Promise;
-
-    /**
-     * Polls the bridge to authorize the app.
-     * @returns {Promise} Resolves with the result of the single request.
-     */
     authorize(): Promise;
 }
 
@@ -107,31 +76,18 @@ interface LightogramBridgeStatic {
 
 interface LightogramLight {
     constructor(bridge: Bridge, lightId: string, lightData: Object);
-
     doApiCall(action: string, endpoint?: string, data?: Object): Promise<any>;
-
     getState(): Promise<HueLightStateRead>;
-
     setState(state: HueLightStateSet, time?: number): any;
-
     setOn(on: boolean, time?: number): any;
-
     setColorRgb(red: number, green: number, blue: number, time?: number): any;
-
     setColorHex(hex: string, time?: number): any;
-
     setColor(color: {}, time?: number): any;
-
     setColorHsb(hue: number, saturation: number, brightness: number, time?: number): any;
-
     setColorTemperature(temperature: number, time?: number): any;
-
     setBrightness(brightness: number, time?: number): any;
-
     getColorRgb(): Promise<{red: {number}, green: {number}, blue: {number}}>;
-
     getColorHex(): Promise<string>;
-
 }
 
 interface LightogramLightStatic {
